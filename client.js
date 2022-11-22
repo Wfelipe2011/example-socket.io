313a8f05b1f6915cbca3c6bde9be42b188a66811
Wilson Felipe <wfelipe2011@gmail.com>
2022-11-22 12:19:48 -0300

    deploy

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://localhost:3000", {});
+const socket = io("http://localhost:3000", {});
 
 socket.on("connect", () => {
   console.log(`connect ${socket.id}`);
